ee9ff6ba36a9759d59d4e05cd546a0e1f8aec0d2
Aadarsha chhetri <152959288+Boredoom17@users.noreply.github.com>
2025-07-20 18:03:31 +0000

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Allow connections from CodeSandbox preview domain
-    allowedHosts: ["c7x5cj-5175.csb.app"],
-  },
 });
