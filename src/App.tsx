3c8a41a862084ef60d878faf6da998da95066498
Aadarsha Chhetri <152959288+Boredoom17@users.noreply.github.com>
2026-02-19 20:17:16 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import SearchOnlyMap from "./components/FlightSearch"; // Note: This was incorrect; we'll fix routing later
+import SearchOnlyMap from "./components/FlightSearch"; 
 
 function App() {
   return (
